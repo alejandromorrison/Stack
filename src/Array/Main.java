0784ea141c9057044174d2baf76720afb81cdf27
alejandro morrison <nelsonmorrisonhidalgo0@gmail.com>
2022-08-26 16:19:19 -0400

    Adding the files

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -1,2 +1,29 @@
-package Array;public class Main {
+package Array;
+
+public class Main {
+    public static void main(String[] args) {
+        // im going to create a stack of 3 items
+        Stack stack = new Stack(3);
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.isFull());
+
+        System.out.println("-------");
+        stack.push(5);
+        stack.push(2);
+        stack.push(9);
+        stack.push(99);
+
+        System.out.println("---------");
+
+        System.out.println(stack.peek());
+
+        stack.pop();
+        stack.pop();
+
+        System.out.println(stack.peek());
+
+        stack.pop();
+
+        System.out.println(stack.peek());
+    }
 }
--- src/Array/Stack.java
+++ src/Array/Stack.java
@@ -1,2 +1,47 @@
-package Array;public class Stack {
+package Array;
+
+// Nelson A. Morrison H.
+
+public class Stack {
+    private final int[] StackItems;
+    public int top;
+    public final int size;
+
+    public Stack(int size) {
+        this.StackItems = new int[size];
+        this.top = -1;
+        this.size = size;
+    }
+
+    // isFull and isEmpty method
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    // going to check is the Stack is full
+    public boolean isFull() {
+        return top == size-1;
+    }
+
+    // we are gonna be working on
+    // push, pop, and peek
+    public void push(int element) {
+        if (!isFull()) {
+            top++;
+            StackItems[top] = element;
+            System.out.println("The element: " + element + " has been inserted on position: " + top);
+        } else System.out.println("The Stack is full");
+    }
+
+    public void pop() {
+        if (isEmpty())  System.out.println("Is empty");
+        else top--;
+    }
+
+    // implementing the peek method
+    public int peek() {
+        if (!isEmpty())
+            return StackItems[top];
+        else return -1;
+    }
 }
